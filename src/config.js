f5c32f219e0f10bdc22afbdea50fa2b1644b9e4f
mjalechuga <32625428+mjalechuga@users.noreply.github.com>
2019-05-15 10:09:03 -0600

    initial commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const clientId = "eca39cf929df498f8dd127acf5044a43";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "user-read-recently-played"
+];
