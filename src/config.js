5fc3f0058a023c4b956c1226afe8260ace23b3ff
MJ <mjalechuga@gmail.com>
2023-09-04 00:42:25 -0600

    remove subfolder path

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 export const clientId = "eca39cf929df498f8dd127acf5044a43";
-export const redirectUri = "https://mjalechuga.github.io/react-spotify-app/top-tracks";
-export const subfolderPath = "/react-spotify-app";
+export const redirectUri = "https://mjalechuga.github.io/react-spotify-app/";
+// export const subfolderPath = "/react-spotify-app";
 
 // #DEV config
 // export const redirectUri = "http://localhost:3000/";
-// export const subfolderPath = "";
+export const subfolderPath = "";
 
 export const scopes = [
   "user-top-read",
