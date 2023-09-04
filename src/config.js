5efe7b05bb727debb8a25527a808603c13f7ccde
MJ <mjalechuga@gmail.com>
2023-09-03 23:12:50 -0600

    Change redirectURI

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 export const clientId = "eca39cf929df498f8dd127acf5044a43";
-export const redirectUri = "https://quincefy.herokuapp.com/";
+export const redirectUri = "https://mjalechuga.github.io/react-spotify-app/";
 // "http://localhost:3000/";
 export const scopes = [
   "user-top-read",
