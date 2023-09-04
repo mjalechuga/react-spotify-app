fdd8e104817df7c5b856baa2c8dff291884f3aac
MJ <mjalechuga@gmail.com>
2023-09-03 22:03:26 -0600

    Updates

---
+++ precache-manifest.9151e94c357c5db4bf16b98848682161.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f3e869042ba0ecac1229f20f3bc430",
+    "url": "/react-spotify-app/index.html"
+  },
+  {
+    "revision": "c28c8b79b26d56670fa3",
+    "url": "/react-spotify-app/static/js/2.60ede610.chunk.js"
+  },
+  {
+    "revision": "4d5e0356022ebac601e5",
+    "url": "/react-spotify-app/static/js/main.e404d82f.chunk.js"
+  },
+  {
+    "revision": "2c42f0b6385eadc96108",
+    "url": "/react-spotify-app/static/js/runtime~main.55b697cf.js"
+  },
+  {
+    "revision": "31bad8d4fc4413d54f8c19930d3739b9",
+    "url": "/react-spotify-app/static/media/Spotify_Logo.31bad8d4.png"
+  }
+]);
